87f55695d65a4cbddec892468ba4268b54c4b9b8
Ruben Astudillo <ruben.astud@gmail.com>
2023-04-24 19:16:31 -0400

    Use a monadic approach with QualifiedDo

--- test/Main.hs
+++ test/Main.hs
@@ -1,9 +1,13 @@
-{-# OPTIONS_GHC -Wno-unused-binds #-}
+{-# Language QualifiedDo #-}
+{-# OPTIONS_GHC -Wno-unused-matches #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Evaluate" #-}
 
 module Main (main) where
 
 import Control.Exception (ErrorCall (..), catch)
-import Data.Forced
+import qualified Data.Forced as DF
+import Data.Forced hiding (pure, fmap, (<*>), return, (>>=), (>>))
 import Data.Map.Lazy qualified as ML
 import Test.HUnit (Counts, Test (TestList), runTestTT, (~:))
 
@@ -24,47 +28,28 @@ errorCalledWithMsg msg io =
 
 noThunksForWHNF :: IO ()
 noThunksForWHNF = do
-    let map0 :: ML.Map Char (ForcedWHNF Int)
-        map0 = ML.empty
-        val0 :: StrictValueExtractor (ForcedWHNF Int)
-        !val0@(Pairy val1 ext) = strictlyWHNF (error "argument evaluated")
-        map1 = ML.insert 'a' (ext val1) map0
+    let map0 :: Demand (ML.Map Char (ForcedWHNF Int))
+        map0 = DF.do
+          v <- demandWHNF (error "argument evaluated")
+          DF.pure $ ML.insert 'a' v ML.empty
+    map1 <- extractDemand map0
     pure ()
 
-noThunksForWHNF1 :: IO ()
-noThunksForWHNF1 = do
-    let map0 :: ML.Map Char (ForcedWHNF Int)
-        map0 = ML.empty
-        val0 :: StrictValueExtractor (ForcedWHNF Int)
-        val0 = strictlyWHNF (error "argument evaluated")
-        val1 = case val0 of Pairy v ext -> ext v
-        map1 = ML.insert 'a' val1 map0
-    pure ()
-
-noThunksForWHNF2 :: IO ()
-noThunksForWHNF2 = do
-    let map0 :: ML.Map Char (ForcedWHNF Int)
-        map0 = ML.empty
-        val1 = case strictlyWHNF (error "argument evaluated") of Pairy v ext -> ext v
-        map1 = ML.insert 'a' val1 map0
-    pure ()
-
+-- | Should not fail.
 thunksForWHNFMaybe :: IO ()
 thunksForWHNFMaybe = do
-    let map0 :: ML.Map Char (ForcedWHNF (Maybe Int))
-        map0 = ML.empty
-        val0 :: StrictValueExtractor (ForcedWHNF (Maybe Int))
-        val0 = strictlyWHNF (const (Just (error "argument evaluated")) 'a')
-        val1 = case val0 of Pairy v ext -> ext v
-        map1 = ML.insert 'a' val1 map0
+    let map0 :: Demand (ML.Map Char (ForcedWHNF (Maybe Int)))
+        map0 = DF.do
+          v <- demandWHNF (const (Just (error "argument evaluated")) 'a')
+          DF.pure $ ML.insert 'a' v ML.empty
+    map1 <- extractDemand map0
     pure ()
 
 noThunksForNFMaybe :: IO ()
 noThunksForNFMaybe = do
-    let map0 :: ML.Map Char (ForcedNF (Maybe Int))
-        map0 = ML.empty
-        val0 :: StrictValueExtractor (ForcedNF (Maybe Int))
-        val0 = strictlyNF ((+ 1) <$> Just (error "argument evaluated" :: Int))
-        val = case val0 of Pairy st ext -> ext st
-        map1 = ML.insert 'a' val map0
+    let map0 :: Demand (ML.Map Char (ForcedNF (Maybe Int)))
+        map0 = DF.do
+          v <- demandNF (const (Just (error "argument evaluated")) 'a')
+          DF.pure $ ML.insert 'a' v ML.empty
+    map1 <- extractDemand map0
     pure ()
